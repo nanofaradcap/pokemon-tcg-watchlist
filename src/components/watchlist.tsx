27c2ae3ad3f2bf2ab612ddeb6a52c00b34a685aa
Chen Zhao <czhao@Chens-Laptop.attlocal.net>
2025-09-26 21:59:17 -0700

    Remove unused useMemo import

--- src/components/watchlist.tsx
+++ src/components/watchlist.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
